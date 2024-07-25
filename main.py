71a9f6d096320af8dfabfd3164a49347bb211108
Vagabond-Midhun <midhungovind624@gmail.com>
2024-07-25 20:58:12 +0530

    25update

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import MySQLdb.cursors,re,hashlib
 
+
+
 app = Flask(__name__)
 
 app.secret_key = 'your secret key'
@@ -10,50 +12,94 @@ app.secret_key = 'your secret key'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '1234'
-app.config['MYSQL_DB'] = 'pythonlogin'
+app.config['MYSQL_DB'] = 'project'
 
 mysql = MySQL(app)
 
-@app.route('/pythonlogin/', methods=['GET', 'POST'])
+
+@app.route('/project/', methods=['GET', 'POST'])
 def login():
+    
     
     msg = ''
    
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         
+        
         username = request.form['username']
         password = request.form['password']
        
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        cursor.execute('SELECT * FROM pythonlogin WHERE username = %s AND password = %s', (username, password,))
        
-        account = cursor.fetchone()
+        pythonlogin = cursor.fetchone()
         
-        if account:
+        if pythonlogin:
            
             session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
+            session['id'] = pythonlogin['id']
+            session['username'] = pythonlogin['username']
+
+            return render_template('main.html', msg=msg)
            
-            return 'Logged in successfully!'
         else:
             
             msg = 'Incorrect username/password!'
     
-    return render_template('main.html', msg=msg)
+    return render_template('index.html')
 
-@app.route('/pythonlogin/logout')
+app.route('/project/logout')
 def logout():
     
    session.pop('loggedin', None)
    session.pop('id', None)
    session.pop('username', None)
    
-   return redirect(url_for('index'))
+   return redirect(url_for('Signin'))
+
+@app.route('/project/register', methods=['GET', 'POST'])
+def register():
+   
+    msg = ''
+    
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM pythonlogin WHERE username = %s', (username,))
+        pythonlogin = cursor.fetchone()
+        
+        if pythonlogin:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+           
+            hash = password + app.secret_key
+            hash = hashlib.sha1(hash.encode())
+            password = hash.hexdigest()
+            
+            cursor.execute('INSERT INTO pythonlogin VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+
+            
+    elif request.method == 'POST':
+       
+        msg = 'Please fill out the form!'
+    
+    return render_template('main.html', msg=msg)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/Signin')
 def Signin():
@@ -67,21 +113,58 @@ def Reg():
 def password():
     return render_template('password.html')
 
-@app.route('/pythonlogin/main')
+@app.route('/project/main')
 def main():
     return render_template('main.html')
 
-@app.route('/pythonlogin/base')
+@app.route('/project/base')
 def base():
     return render_template('base.html')
 
-@app.route('/pythonlogin/profile')
+@app.route('/project/profile')
 def profile():
     return render_template('profile.html')
 
-@app.route('/pythonlogin/marriage')
+@app.route('/project/marriage')
 def marriage():
     return render_template('marriage.html')
+
+@app.route('/project/HIMTEE')
+def HIMTEE():
+    return render_template('HIMTEE.html')
+
+@app.route('/project/content3')
+def content3():
+    return render_template('content3.html')
+
+@app.route('/project/content4')
+def content4():
+    return render_template('content4.html')
+
+@app.route('/project/content5')
+def content5():
+    return render_template('content5.html')
+
+@app.route('/project/content6')
+def content6():
+    return render_template('content6.html')
+
+@app.route('/project/content7')
+def content7():
+    return render_template('content7.html')
+
+@app.route('/project/content8')
+def content8():
+    return render_template('content8.html')
+
+@app.route('/project/content9')
+def content9():
+    return render_template('content9.html')
+
+@app.route('/project/content10')
+def content10():
+    return render_template('content10.html')
+
 
 if __name__ == '__main__':
     app.run()
